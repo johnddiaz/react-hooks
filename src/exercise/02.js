510d2cd8bc573ba6db52866900b7b95fdd637c82
John Diaz <jdiaz@dwelo.com>
2021-01-23 23:03:52 -0700

    lessons 1-4

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,11 +3,23 @@
 
 import * as React from 'react'
 
+function useLocalStorageState(key, fallbackValue = '') {
+  const [value, setValue] = React.useState(() => {
+    const lsValue = window.localStorage.getItem(key);
+    return lsValue ? JSON.parse(lsValue) : fallbackValue
+  });
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, window.JSON.stringify(value));
+  }, [key, value])
+
+  return [value, setValue];
+}
+
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
-
+  const [name, setName] = useLocalStorageState('name', initialName)
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
@@ -19,7 +31,7 @@ function Greeting({initialName = ''}) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
